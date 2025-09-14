a3d102fb80fcdad5d1f370f07188c78c254db43e
tino669244 <tinoruine@gmail.com>
2025-09-14 17:35:31 +0300

    Update index.py
    Signed-off-by: tino669244 <tinoruine@gmail.com>

--- api/index.py
+++ api/index.py
@@ -1,5 +1,15 @@
-from main import app as application
+"""
+Vercel entrypoint ho an'ny Flask app.
+Mitady ilay app ao amin'ny main.py.
+"""
 
-# Vercel handler
-def handler(request, *args, **kwargs):
-    return application(request, *args, **kwargs)
+import sys
+import os
+
+# Raha mila mampiditra modules avy any ivelany (raha misy subfolder)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from main import app as application  # misintona ilay app avy amin'ny main.py
+
+# Vercel mitady "app"
+app = application
